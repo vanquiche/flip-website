ec66c1d6ded85963e544a6ba14992652a93d1516
vanquiche <stevevang2010@hotmail.com>
2022-09-08 16:09:49 +0900

    add custom font

---
+++ pages/_document.tsx
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link
+          href='https://fonts.googleapis.com/css?family=Baloo+2:regular,500,600,700,800'
+          rel='stylesheet'
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
