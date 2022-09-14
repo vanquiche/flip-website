84ef144b9358dd390a3373e4b4e2e27dc9152eb5
vanquiche <stevevang2010@hotmail.com>
2022-09-14 19:50:31 +0900

    removed unneeded font preconnect

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,17 +1,10 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 import loader from '../components/loader';
-import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang='en'>
       <Head>
-        <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link
-          rel='preconnect'
-          href='https://fonts.gstatic.com'
-          crossOrigin='true'
-        />
         <link
           href='https://fonts.googleapis.com/css?family=Baloo+2:regular,500,600,700,800&display=swap'
           rel='stylesheet'
